715ef02213a01a069aa2ab117a9ebd5638bf1241
Alcosmos <laura@TurboSoyuz.localdomain>
2021-06-29 01:27:14 +0200

    v1

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+	// Clan webs By Alcosmos - https://alcosmos.ddns.net
+	// https://github.com/Alcosmos/clash-of-clans-clanwebs
+	// Read the README.md
+	
+	
+	
+	// Available languages: es en
+	// You can add your own languate modifying data/languages.json
+	$defaultLang='es';
+	
+	// Search engine results
+	$description='A Clash of Clans\' Clan webs';
+	$keywords='game games juego juegos clash of clans royale alcosmos war wars guerra guerras de clan clanes';
+	
+	// URL to Clash of Clans API
+	$apiUrl = 'https://api.clashofclans.com/v1/clans/%23';
+	
+	// Don't include the #
+	$clanTag='';
+	
+	// Your Supercell developer key - https://developer.clashofclans.com
+	$apiKey = '';
+?>
